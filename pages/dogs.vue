be513228f5e82da6c6f9d56e657d870f25298c26
Ashlyn Knox <ashlyn@lilyx.io>
2024-01-18 11:05:46 -0700

    added notes for what code is doing in the dog data files

---
+++ pages/dogs.vue
@@ -0,0 +1,16 @@
+<script setup>
+// Fetch data from the dogs API endpoint
+</script>
+<template>
+  <main>
+    <header>
+      <h1>Dog Breeds</h1>
+    </header>
+    <div>
+      <ul>
+        <!-- Loop through the data to show all dog breeds -->
+        <li></li>
+      </ul>
+    </div>
+  </main>
+</template>
--- server/api/dogs.js
+++ server/api/dogs.js
@@ -1,9 +1,15 @@
 export default defineEventHandler((event) => {
+  // declare function to fetch data from the dog api
   async function getDogData() {
+    // fetch data from the external endpoint
     const res = await fetch("https://dog.ceo/api/breeds/list/all");
+    // parse the data so JS can read it
     const dogData = await res.json();
+    // return the data from the function
     return dogData;
   }
+  // invoke the function and assign it's output to the data variable
   const data = getDogData();
+  // return the data at the endpoint
   return data;
 });
